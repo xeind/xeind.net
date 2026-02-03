c5be503fdb592a64f9fe03163a4aa9231d7ca730
Xein Deniel <xeindeniel@gmail.com>
2026-02-03 20:26:35 +0800

    feat: on hover logo outline

--- components/XeinLogo.tsx
+++ components/XeinLogo.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { memo } from "react";
 
 interface XeinLogoProps {
@@ -7,40 +9,91 @@ interface XeinLogoProps {
 
 function XeinLogoComponent({ className = "", size = 48 }: XeinLogoProps) {
   return (
-    <svg
-      viewBox="0 0 7084 7553"
-      width={size}
-      height={size}
-      className={className}
-      aria-label="Xein Logo"
+    <div
+      className={`xein-logo ${className}`}
+      style={{ width: size, height: size, position: "relative" }}
       role="img"
-      focusable="false"
+      aria-label="Xein Deniel Logo"
     >
-      {/* Left side "X" - Individual hover on this group */}
-      <g className="opacity-90 transition-opacity duration-300 ease-out hover:opacity-100">
-        <polygon
-          className="fill-accent-hover"
-          points="2195 3776 2195 3776 764 2345 1394 1981 1394 1099 0 1904 0 2662 1115 3776 1115 3776 2533 5195 2533 6229 1881 5853 1881 6735 3297 7553 3298 4879 2195 3776"
-        />
-        <polygon
-          className="fill-accent"
-          points="2195 3776 3298 2674 3297 0 1881 818 1881 1700 2533 1324 2533 2358 1115 3776 1115 3776 0 4891 0 5649 1394 6454 1394 5571 764 5207 2195 3776 2195 3776"
-        />
-      </g>
-      {/* Right side "D" - Individual hover on this group */}
-      <g className="opacity-90 transition-opacity duration-300 ease-out hover:opacity-100">
-        {/* Top-right quadrant */}
-        <polygon
-          className="fill-accent"
-          points="7084 1905 5689 1100 5689 1982 6320 2346 6320 3776 6320 5206 5689 5571 5689 6453 7084 5648 7084 3776 7084 1905"
-        />
-        {/* Bottom-right quadrant */}
-        <polygon
-          className="fill-accent"
-          points="4549 1324 5202 1701 5202 818 3784 0 3784 2876 2884 3777 3784 4677 3784 7553 5202 6735 5202 5852 4549 6229 4549 4361 3965 3777 4549 3192 4549 1324"
-        />
-      </g>
-    </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 7083.94 7553"
+        width={size}
+        height={size}
+        style={{ position: "absolute", top: 0, left: 0 }}
+      >
+        {/* Shape 1 - X top polygon (cls-1) */}
+        <g className="logo-shape" data-shape="1">
+          <polygon
+            className="shape-filled"
+            fill="currentColor"
+            points="2195.38 3776.32 3297.59 2674.1 3297.4 0 1881.17 817.66 1881.17 1700.09 2533.38 1323.54 2533.38 2357.55 1114.63 3776.31 1114.62 3776.31 0 4890.93 0 5648.88 1394.13 6453.77 1394.13 5571.34 764.22 5207.47 2195.38 3776.32 2195.38 3776.32"
+          />
+          <path
+            className="shape-outline stroke-accent/30"
+            d="M2195.38 3776.32L3297.59 2674.1L3297.4 0L1881.17 817.66V1700.09L2533.38 1323.54V2357.55L1114.63 3776.31H1114.62L0 4890.93V5648.88L1394.13 6453.77V5571.34L764.22 5207.47L2195.38 3776.32Z"
+            strokeWidth="50"
+            strokeLinecap="square"
+            strokeDasharray="120 120"
+            fill="none"
+          />
+        </g>
+
+        {/* Shape 2 - X bottom polygon (cls-2) */}
+        <g className="logo-shape" data-shape="2">
+          <polygon
+            className="shape-filled"
+            fill="currentColor"
+            opacity="0.7"
+            points="2195.37 3776.32 2195.38 3776.32 764.21 2345.15 1394.12 1981.29 1394.12 1098.86 0 1903.75 0 2661.7 1114.62 3776.32 1114.61 3776.32 2533.38 5195.08 2533.38 6229.09 1881.16 5852.54 1881.16 6734.97 3297.39 7552.63 3297.59 4878.53 2195.37 3776.32"
+          />
+          <path
+            className="shape-outline stroke-accent/30"
+            d="M2195.37 3776.32H2195.38L764.21 2345.15L1394.12 1981.29V1098.86L0 1903.75V2661.7L1114.62 3776.32H1114.61L2533.38 5195.08V6229.09L1881.16 5852.54V6734.97L3297.39 7552.63L3297.59 4878.53L2195.37 3776.32Z"
+            strokeWidth="50"
+            strokeLinecap="square"
+            strokeDasharray="120 120"
+            fill="none"
+          />
+        </g>
+
+        {/* Shape 3 - D right polygon (cls-3) */}
+        <g className="logo-shape" data-shape="3">
+          <polygon
+            className="shape-filled"
+            fill="currentColor"
+            opacity="0.7"
+            points="7083.94 1904.96 5689.01 1099.6 5689.01 1982.03 6319.7 2346.16 6319.7 3776.32 6319.7 5206.48 5689.01 5570.6 5689.01 6453.04 7083.94 5647.67 7083.91 3776.32 7083.94 1904.96"
+          />
+          <path
+            className="shape-outline stroke-accent/30"
+            d="M7083.94 1904.96L5689.01 1099.6V1982.03L6319.7 2346.16V3776.32V5206.48L5689.01 5570.6V6453.04L7083.94 5647.67L7083.91 3776.32L7083.94 1904.96Z"
+            strokeWidth="50"
+            strokeLinecap="square"
+            strokeDasharray="120 120"
+            fill="none"
+          />
+        </g>
+
+        {/* Shape 4 - D left polygon (cls-4) */}
+        <g className="logo-shape" data-shape="4">
+          <polygon
+            className="shape-filled"
+            fill="currentColor"
+            opacity="0.7"
+            points="4549 1323.84 5201.96 1700.83 5201.96 818.4 3784.45 0 3784.45 2876.03 2883.98 3776.5 3784.45 4676.97 3784.45 7553 5201.96 6734.6 5201.96 5852.17 4549 6229.16 4549 4360.76 3964.74 3776.5 4549 3192.24 4549 1323.84"
+          />
+          <path
+            className="shape-outline stroke-accent/30"
+            d="M4549 1323.84L5201.96 1700.83V818.4L3784.45 0V2876.03L2883.98 3776.5L3784.45 4676.97V7553L5201.96 6734.6V5852.17L4549 6229.16V4360.76L3964.74 3776.5L4549 3192.24V1323.84Z"
+            strokeWidth="50"
+            strokeLinecap="square"
+            strokeDasharray="120 120"
+            fill="none"
+          />
+        </g>
+      </svg>
+    </div>
   );
 }
 
